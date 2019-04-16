1dff92c2bc4c6b80c9b1cf313d9a6ba9af9ac8af
Tocha <emerson.barea@gmail.com>
2019-04-15 23:01:27 -0300

    comment

--- install.sh
+++ install.sh
@@ -50,8 +50,8 @@ function install_metis() {
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
     sudo -u $USER rm -rf $INSTALL_DIR/metis-5.1.0* 2> /dev/null
     
-    sudo -u $USER wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $BUILD_DIR
-    #sudo -u $USER wget http://192.168.56.254/metis-5.1.0.tar.gz -P $BUILD_DIR
+    sudo -u $USER wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $INSTALL_DIR
+    #sudo -u $USER wget http://192.168.56.254/metis-5.1.0.tar.gz -P $INSTALL_DIR
     
     sudo -u $USER tar -xvzf $INSTALL_DIR/metis-5.1.0.tar.gz -C $INSTALL_DIR
     cd $INSTALL_DIR/metis-5.1.0
