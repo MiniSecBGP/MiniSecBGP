8061395cdbe5c1b5f3ff98fa158bd4e2b57afcdb
Tocha <emerson.barea@gmail.com>
2019-04-15 21:49:28 -0300

    comment

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# MiniSecBGP install script for Ubuntu 18.04.1 Server LTS (Bionic Beaver)
+# Emerson Barea (emerson.barea@gmail.com)
+
+function welcome() {
+    printf '\n%s\n' 'This script creates the complete environment for MiniSecBGP.
+                   Some requirements must be met:
+                   - Ubuntu 18.04.1 Server LTS (Bionic Beaver)
+                   - 2 NICs (one for administration and other for cluster nodes network communication)
+                   - Internet access
+                   - Initial user with "sudo" rights
+                   Note: the user "minisecbgp" will be created, so it should not exist previously.'
+}
+
