9fa6643d9974b9c77e64553cb0489113473affe0
Tocha <emerson.barea@gmail.com>
2019-04-15 22:56:17 -0300

    comment

--- install.sh
+++ install.sh
@@ -13,6 +13,16 @@ function welcome() {
                    Note: the user "minisecbgp" will be created, so it should not exist previously.'
 }
 
+function qtd_hosts() {
+    printf '\n%s' 'How many cluster nodes do you want to create? (write only numbers. Ex.: 3): '
+    read var_qtd_hosts
+    re='^[0-9]+$'
+    if ! [[ $var_qtd_hosts =~ $re ]] ; then
+        printf '\e[1;31m%-6s\e[m' 'error: Write only numbers'
+        qtd_hosts;
+    fi
+}
+
 function update_SO_install_packages() {
     printf '\n\e[1;33m%-6s\e[m\n' '-- Installing SO updates and packages install...'
     printf '\e[1;33m%-6s\e[m\n' 'Please, confirm if the computer has internet connectivity.'
@@ -34,17 +44,17 @@ function network_configuration() {
 }
 
 function install_metis() {
-    printf '\e[1;33m%-6s%s\e[m\n' 'Installing METIS in ' $BUILD_DIR/metis
+    printf '\e[1;33m%-6s%s\e[m\n' 'Installing METIS in ' $INSTALL_DIR/metis
     printf '\n\e[1;33m%-6s\e[m\n' 'Resolving requirements'
     sudo apt install cmake -y
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
-    sudo -u mininet rm -rf $BUILD_DIR/metis-5.1.0* 2> /dev/null
+    sudo -u mininet rm -rf $INSTALL_DIR/metis-5.1.0* 2> /dev/null
     
-    #sudo -u mininet wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $BUILD_DIR
-    sudo -u mininet wget http://192.168.56.254/metis-5.1.0.tar.gz -P $BUILD_DIR
+    sudo -u mininet wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $BUILD_DIR
+    #sudo -u mininet wget http://192.168.56.254/metis-5.1.0.tar.gz -P $BUILD_DIR
     
-    sudo -u mininet tar -xvzf $BUILD_DIR/metis-5.1.0.tar.gz -C $BUILD_DIR
-    cd $BUILD_DIR/metis-5.1.0
+    sudo -u $USER tar -xvzf $INSTALL_DIR/metis-5.1.0.tar.gz -C $INSTALL_DIR
+    cd $INSTALL_DIR/metis-5.1.0
     sudo make config shared=1
     sudo make
     sudo make install
@@ -59,17 +69,17 @@ function install_app_maxinet() {
     install_metis;
 
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
-    sudo -u mininet rm -rf $BUILD_DIR/maxinet
-    sudo -u mininet git clone git://github.com/MaxiNet/MaxiNet.git $BUILD_DIR/MaxiNet
-    cd $BUILD_DIR/MaxiNet
-    sudo -u mininet git checkout v1.2
+    sudo -u $USER rm -rf $INSTALL_DIR/maxinet
+    sudo -u $USER git clone git://github.com/MaxiNet/MaxiNet.git $INSTALL_DIR/MaxiNet
+    cd $INSTALL_DIR/MaxiNet
+    sudo -u $USER git checkout v1.2
     sudo make install
-    sudo cp $BUILD_DIR/MaxiNet/share/MaxiNet-cfg-sample /etc/MaxiNet.cfg
+    sudo cp $INSTALL_DIR/MaxiNet/share/MaxiNet-cfg-sample /etc/MaxiNet.cfg
     printf '\e[1;33m%-6s\e[m\n' 'Configuring MaxiNet config file.'
     sudo sed -i -- 's/password = HalloWelt/password = abc123/g' /etc/MaxiNet.cfg
     sudo sed -i -- 's/controller = 192.168.123.1:6633/controller = 192.168.254.1:6633/g' /etc/MaxiNet.cfg
     sudo sed -i -- 's/logLevel = INFO/logLevel = ERROR/g' /etc/MaxiNet.cfg
-    sudo sed -i -- 's/sshuser = root/sshuser = mininet/g' /etc/MaxiNet.cfg
+    sudo sed -i -- 's/sshuser = root/sshuser = $USER/g' /etc/MaxiNet.cfg
     sudo sed -i -- 's/usesudo = False/usesudo = True/g' /etc/MaxiNet.cfg
     sudo sed -i -- 's/ip = 192.168.123.1/ip = 192.168.254.1/g' /etc/MaxiNet.cfg
     sudo sed -i -- 19,'$d' /etc/MaxiNet.cfg
@@ -79,10 +89,12 @@ function install_app_maxinet() {
 }
 
 # set up build directory
-LOCAL_DIR=$(pwd)
-BUILD_DIR=/home/mininet
-MINISECBGP_DIR=$BUILD_DIR/MiniSecBGP
+USER=$(whoami)
+HOME_DIR=$(echo $HOME)
+INSTALL_DIR=$HOME_DIR/MiniSecBGP
 
 welcome;
+qtd_hosts;
 update_SO_install_packages;
-#network_configuration;
+network_configuration;
+install_app_maxinet;
